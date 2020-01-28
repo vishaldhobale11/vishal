ba64bf9d6027d4867bd4fe612b82e8a951ad37de
Vishal Dhobale <vishaldhobale11@gmail.com>
2020-01-28 14:11:20 +0530

    Added some feature

--- hungry.py
+++ hungry.py
@@ -1,9 +1,11 @@
 hungry=input("are you hungry?")
 if hungry=="yes":
-
-print("eat dinner")
+    print("eat dinner")
+    
+    
 
 else:
-    thrusty=("are you thrusty?")
-    if thrusty=="yes":
-    print'{do your homework}'
+        thrusty=("are you thrusty?")
+        if thrusty=="yes":
+            print("Drink water")
+print("do your homework")
